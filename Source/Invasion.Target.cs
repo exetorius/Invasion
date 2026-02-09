48b05ea609907f6e51e33545e68a9c085c0904a5
exetorius <gee85@hotmail.co.uk>
2026-02-09 21:52:47 +0000

    Fixed so the project really runs in 5.7

--- Source/Invasion.Target.cs
+++ Source/Invasion.Target.cs
@@ -8,8 +8,8 @@ public class InvasionTarget : TargetRules
 	public InvasionTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("Invasion");
 	}
 }
