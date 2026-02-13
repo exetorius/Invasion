afa614c55f05100bb6587bb5d6605df32e09d11e
exetorius <gee85@hotmail.co.uk>
2026-02-13 22:45:12 +0000

    Basic steam lobby created

--- Source/Invasion/Public/GameInstance/InvasionGameInstance.h
+++ Source/Invasion/Public/GameInstance/InvasionGameInstance.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AdvancedFriendsGameInstance.h"
 #include "Engine/GameInstance.h"
 #include "InvasionGameInstance.generated.h"
 
@@ -10,7 +11,7 @@
  * 
  */
 UCLASS()
-class INVASION_API UInvasionGameInstance : public UGameInstance
+class INVASION_API UInvasionGameInstance : public UAdvancedFriendsGameInstance
 {
 	GENERATED_BODY()
 	
